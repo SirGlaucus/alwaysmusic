7e5b8159e28059359a688da8b84e44072c657bc9
SirGlaucus <glaucusjoseph@gmail.com>
2022-05-23 22:05:45 -0400

    Requerimientos terminados

--- consultas.js
+++ consultas.js
@@ -7,31 +7,42 @@ const config = {
     password: "1234",
     port: 5432,
 }
-
+// 1. Realizar la conexión con PostgreSQL con la clase Client.
 const client = new Client(config)
 client.connect()
 
+//2. Crear una función asíncrona para registrar un nuevo estudiante en la base de datos.
 const nuevo = async (nombre, rut, curso, nivel) => {
     const res = await client.query(`INSERT INTO alumnos (nombre, rut, curso, nivel) VALUES ('${nombre}', '${rut}', '${curso}', ${nivel});`)
     console.log(`Estudiante ${nombre} agregado con exito`)
     client.end()
 }
 
+// 4. Crear una función asíncrona para obtener por consola todos los estudiantes registrados.
 const consulta = async () => {
     const res = await client.query("SELECT * FROM alumnos")
     console.log('Registro actual: ', res.rows)
     client.end()
 }
 
+// 5. Crear una función asíncrona para actualizar los datos de un estudiante en la base de datos.
 const editar = async (nombre, rut, curso, nivel) => {
     const res = await client.query(`UPDATE alumnos SET nombre = '${nombre}', curso = '${curso}', nivel = ${nivel} WHERE rut = '${rut}';`)
     console.log(`Estudiante ${nombre} editado con exito`)
     client.end()
 }
 
+// 3. Crear una función asíncrona para obtener por consola el registro de un estudiante por medio de su rut.
 const rut = async (rut) => {
     const res = await client.query(`SELECT * FROM alumnos WHERE rut = '${rut}'`)
     console.log(res.rows)
+    client.end()
+}
+
+// 6. Crear una función asíncrona para eliminar el registro de un estudiante de la base de datos.
+const eliminar = async (rut) => {
+    const res = await client.query(`DELETE FROM alumnos WHERE rut = '${rut}'`)
+    console.log(`Registro de estudiante con rut ${rut} eliminado`)
     client.end()
 }
 
@@ -39,5 +50,6 @@ module.exports = {
     nuevo,
     consulta,
     editar,
-    rut
+    rut,
+    eliminar
 }
--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const argumentos = process.argv
 
-const {consulta, nuevo, editar, rut} = require('./consultas')
+const { rut, consulta, nuevo, editar, eliminar } = require('./consultas')
 
-const acccionSql = argumentos[2] // NUEVO, obtener 1, obtener todos, eliminar, editar
-const param1 = argumentos[3] //
+const acccionSql = argumentos[2] // Se utiliza para saber que funcion vamos a utilizar (nuevo, consulta, editar, rut o eliminar)
+const param1 = argumentos[3] // Este parametro puede tener como valor el nombre (nuevo, editar) o el rut (rut, eliminar)
 const param2 = argumentos[4]
 const param3 = argumentos[5]
 const param4 = argumentos[6]
 
 switch (acccionSql) {
+    case 'nuevo':
+        nuevo(param1, param2, param3, param4)
+        break
     case 'consulta':
         consulta()
-        break
-    case 'crear':
-        nuevo(param1, param2, param3, param4)
         break
     case 'editar':
         editar(param1, param2, param3, param4)
         break
     case 'rut':
         rut(param1)
+        break
+    case 'eliminar':
+        eliminar(param1)
         break
     default:
         console.log('Error: funcion no especificada o mal escrita.')
